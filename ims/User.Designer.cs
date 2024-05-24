e5ace77088eab5088875eb8d93b19afde8d8dfbd
Salsoft - Saad Hassan <m.saadhassan@salsoft.net>
2024-05-24 12:51:59 +0500

    User From Added
    User From Added

--- ims/User.Designer.cs
+++ ims/User.Designer.cs
@@ -28,8 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel5 = new Panel();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            panel6 = new Panel();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            panel7 = new Panel();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            panel8 = new Panel();
+            textBox5 = new TextBox();
+            label6 = new Label();
+            panel9 = new Panel();
+            textBox6 = new TextBox();
+            label7 = new Label();
             leftPanel.SuspendLayout();
             panel1.SuspendLayout();
+            panel5.SuspendLayout();
+            panel6.SuspendLayout();
+            panel7.SuspendLayout();
+            panel8.SuspendLayout();
+            panel9.SuspendLayout();
             SuspendLayout();
             // 
             // backBtn
@@ -37,18 +57,194 @@
             backBtn.FlatAppearance.BorderSize = 0;
             backBtn.Click += backBtn_Click;
             // 
+            // leftPanel
+            // 
+            leftPanel.Controls.Add(panel9);
+            leftPanel.Controls.Add(panel8);
+            leftPanel.Controls.Add(panel7);
+            leftPanel.Controls.Add(panel6);
+            leftPanel.Controls.Add(panel5);
+            leftPanel.Controls.SetChildIndex(panel1, 0);
+            leftPanel.Controls.SetChildIndex(panel5, 0);
+            leftPanel.Controls.SetChildIndex(panel6, 0);
+            leftPanel.Controls.SetChildIndex(panel7, 0);
+            leftPanel.Controls.SetChildIndex(panel8, 0);
+            leftPanel.Controls.SetChildIndex(panel9, 0);
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(textBox2);
+            panel5.Controls.Add(label3);
+            panel5.Location = new Point(12, 137);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(211, 47);
+            panel5.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Dock = DockStyle.Top;
+            label3.Location = new Point(0, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 20);
+            label3.TabIndex = 0;
+            label3.Text = "Name";
+            // 
+            // textBox2
+            // 
+            textBox2.Dock = DockStyle.Bottom;
+            textBox2.Location = new Point(0, 20);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(211, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // panel6
+            // 
+            panel6.Controls.Add(textBox3);
+            panel6.Controls.Add(label4);
+            panel6.Location = new Point(12, 190);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(211, 47);
+            panel6.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Dock = DockStyle.Bottom;
+            textBox3.Location = new Point(0, 20);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(211, 27);
+            textBox3.TabIndex = 1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Dock = DockStyle.Top;
+            label4.Location = new Point(0, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(75, 20);
+            label4.TabIndex = 0;
+            label4.Text = "Username";
+            // 
+            // panel7
+            // 
+            panel7.Controls.Add(textBox4);
+            panel7.Controls.Add(label5);
+            panel7.Location = new Point(12, 243);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(211, 47);
+            panel7.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            textBox4.Dock = DockStyle.Bottom;
+            textBox4.Location = new Point(0, 20);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(211, 27);
+            textBox4.TabIndex = 1;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Dock = DockStyle.Top;
+            label5.Location = new Point(0, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(70, 20);
+            label5.TabIndex = 0;
+            label5.Text = "Password";
+            // 
+            // panel8
+            // 
+            panel8.Controls.Add(textBox5);
+            panel8.Controls.Add(label6);
+            panel8.Location = new Point(12, 296);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(211, 47);
+            panel8.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            textBox5.Dock = DockStyle.Bottom;
+            textBox5.Location = new Point(0, 20);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(211, 27);
+            textBox5.TabIndex = 1;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Dock = DockStyle.Top;
+            label6.Location = new Point(0, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(46, 20);
+            label6.TabIndex = 0;
+            label6.Text = "Email";
+            // 
+            // panel9
+            // 
+            panel9.Controls.Add(textBox6);
+            panel9.Controls.Add(label7);
+            panel9.Location = new Point(12, 349);
+            panel9.Name = "panel9";
+            panel9.Size = new Size(211, 47);
+            panel9.TabIndex = 3;
+            // 
+            // textBox6
+            // 
+            textBox6.Dock = DockStyle.Bottom;
+            textBox6.Location = new Point(0, 20);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(211, 27);
+            textBox6.TabIndex = 1;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Dock = DockStyle.Top;
+            label7.Location = new Point(0, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(50, 20);
+            label7.TabIndex = 0;
+            label7.Text = "Phone";
+            // 
             // User
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
+            Margin = new Padding(3, 7, 3, 7);
             Name = "User";
             Text = "User";
             leftPanel.ResumeLayout(false);
             panel1.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            panel7.ResumeLayout(false);
+            panel7.PerformLayout();
+            panel8.ResumeLayout(false);
+            panel8.PerformLayout();
+            panel9.ResumeLayout(false);
+            panel9.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel5;
+        private TextBox textBox2;
+        private Label label3;
+        private Panel panel9;
+        private TextBox textBox6;
+        private Label label7;
+        private Panel panel8;
+        private TextBox textBox5;
+        private Label label6;
+        private Panel panel7;
+        private TextBox textBox4;
+        private Label label5;
+        private Panel panel6;
+        private TextBox textBox3;
+        private Label label4;
     }
 }
